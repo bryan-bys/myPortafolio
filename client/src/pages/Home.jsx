6bbe785faefd0bc3476e6eec821bb3f12b884b8e
bryan-bys <bryanmm212@gmail.com>
2024-09-04 18:40:54 +0200

    update home section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,17 +1,17 @@
-
 const Home = ({ homeActive }) => {
   return (
     <div className={`home-container ${homeActive ? "home-active" : null}`}>
       <div className="home-content">
         <h3>WELCOME</h3>
         <p>
-          Hola, soy Bryan, un apasionado del desarrollo web autodidacta con más
-          de dos años de experiencia. Me especializo en frontend y también tengo
-          conocimientos en backend. En este portafolio, encontrarás una muestra
-          de mis proyectos, donde podrás ver mi dedicación y amor por la
-          creación de interfaces intuitivas y atractivas, así como mi habilidad
-          para desarrollar soluciones en el backend. ¡Espero que disfrutes
-          explorando mi trabajo tanto como yo disfruté creándolo!
+          Hola, soy Bryan, un apasionado del desarrollo web autodidacta con
+          solidos conocimientos en React, typescript,Javascript y css. También
+          tengo conocimientos en backend con Python y Django. En este
+          portafolio, encontrarás una muestra de mis proyectos, donde podrás ver
+          mi dedicación y amor por la creación de interfaces intuitivas y
+          atractivas, así como mi habilidad para desarrollar soluciones en el
+          backend. ¡Espero que disfrutes explorando mi trabajo tanto como yo
+          disfruté creándolo!
         </p>
       </div>
     </div>
